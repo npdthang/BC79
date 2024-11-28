459f1dfa81de5396ae144d92f49eddd409d33468
npdthang <npdthang1991@gmail.com>
2024-11-28 18:41:35 +0700

    Add js folder

---
+++ JS-1/Base/index.js
@@ -0,0 +1,24 @@
+console.log("Hello Bc-79");
+
+// Tạo biến và gán giá trị
+// let name = value;
+
+let userName = "Thang";
+let age = 33;
+let isMale = true;
+let isMarried = null;
+let isGood = undefined;
+// 5 kiểu dữ liệu cơ bản
+
+// in ra giá trị
+console.log(`tên là ${userName}`);
+
+// cập nhật giá trị của biến
+userName = "Thang 2";
+
+// toán tử
+
+let n3 = 2 + 2;
+
+// ++a cộng r in
+// a++ in rồi mới cộng, lần sau mới tăng
