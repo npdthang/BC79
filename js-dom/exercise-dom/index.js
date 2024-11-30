a4e6ca007c6019b920a906070f6ce3cb24f758b0
npdthang <npdthang1991@gmail.com>
2024-12-01 06:15:35 +0700

    Add folder

---
+++ js-dom/exercise-dom/index.js
@@ -0,0 +1,4 @@
+let username = document.querySelector("#txt-username");
+let password = document.querySelector("#txt-password");
+
+username.value = "admin";
---
+++ js-dom/index.js
@@ -0,0 +1,5 @@
+console.log("hello");
+
+let logo = document.querySelector("h1");
+
+logo.style.color = "red";
